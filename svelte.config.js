863a446e97dcaa7364af93effe0908c47ebfe84d
euromoon <lucasgarrafielo04@gmail.com>
2022-01-10 16:28:22 -0300

    nojekyll

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,14 +12,20 @@ const config = {
   // procuro uma configuração que remova o path absoluto da build final,
   // por enquanto, estou removendo manualmente.
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
     ssr: false,
     floc: false,
     paths: {
-      base: '/musicolor'
-    }
+      base: dev ? '' : '/musicolor',
+    },
+    appDir: 'app_',
   },
 };
 
